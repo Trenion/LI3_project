c5b78b8ad116ec2031808a7b8ebb0f16ce62dda2
Trenion <49897318+Trenion@users.noreply.github.com>
2020-02-12 19:37:10 +0000

    Add files via upload

---
+++ prótotipo_1.c
@@ -0,0 +1,23 @@
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+int PraAnalisarFicheiro (){
+	char *nomeDoFicheiroAUsar;
+	printf("Qual é o ficheiro a analisar?\n");
+	scanf("%s",&nomeDoFicheiroAUsar);
+	ficheiroAUsar = fopen(nomeDoFicheiroAUsar,"r");
+	
+
+
+
+	fclose(fp);
+	return(0);
+}
+
