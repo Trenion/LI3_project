d88ddf9a3c3c074200e5dfe5febb7a08827042fa
Trenion <ggrgib@gmail.com>
2020-03-04 17:45:59 +0000

    interface3

--- interface.h
+++ interface.h
@@ -7,16 +7,16 @@
 
 
 SGV initSGV();
-SGV destroySGV();
+SGV destroySGV();/////////////////////
 SGV loadSGVFromFiles(SGV sgv, char* filesFolderPath);
 <?> getProductsStartedByLetter(SGV sgv, char letter);
 <?> getProductSalesAndProfit(SGV sgv, char* productID, int month);
-<?> getProductsNeverBought(SGV sgv, int branchID);
-<?> getClientsOfAllBranches(SGV sgv);
-<?> getClientsAndProductsNeverBoughtCount(SGV sgv);
-<?> getProductsBoughtByClient(SGV sgv, char* clientID);
+<?> getProductsNeverBought(SGV sgv, int branchID);/////////////////////
+<?> getClientsOfAllBranches(SGV sgv);/////////////////////
+<?> getClientsAndProductsNeverBoughtCount(SGV sgv);/////////////////////
+<?> getProductsBoughtByClient(SGV sgv, char* clientID);/////////////////////
 <?> getSalesAndProfit(SGV sgv, int minMonth, int maxMonth);
 <?> getProductBuyers(SGV sgv, char* productID, int branch);
-<?> getClientFavoriteProduct(SGV sgv, char* clientID, int branch);
-<?> getTopSelledProducts(SGV sgv, int limit);
-<?> getClientTopProfitProducts(SGV sgv, char* clientID, int limit);
+<?> getClientFavoriteProduct(SGV sgv, char* clientID, int branch);/////////////////////
+<?> getTopSelledProducts(SGV sgv, int limit);/////////////////////
+<?> getClientTopProfitProducts(SGV sgv, char* clientID, int limit);/////////////////////
